5246e53e5fce65470f9ce2773213c90ddd9f383c
Niracler <1026037967@qq.com>
2019-08-30 11:04:24 +0800

    使用postgresql后好像的确有优化

--- apps/news/fakes.py
+++ apps/news/fakes.py
@@ -6,10 +6,10 @@
 3. 批量插入，没有标签，无视冲突，有冲突的插入失败 43秒
 4. 批量插入，有标签 26分钟？？
 5. 批量插入，有1标签 26分钟？？25626
+6. 使用 postgres， 4分钟
 
 """
 
-import base64
 import random
 import pytz
 from faker import Faker
@@ -21,27 +21,21 @@ fake = Faker("zh_CN")
 def fake_article(count=50):
     """生成假文章"""
 
-    article_list = []
-
-    try:
-        for i in range(count):
-            url = fake.uri()
+    for i in range(count):
+        try:
             article = Article(
-                id=i + 129970,
                 website_name=fake.name(),  # 来源网站的名称
                 url=fake.uri(),  # 文章链接
                 title=fake.sentence(),  # 文章标题
                 content=fake.text(2000),  # 文章内容
                 publish_time=fake.date_time_this_year(tzinfo=pytz.UTC)
             )
+            article.save()
             for i in range(random.randint(1, 4)):
                 article.tags.add(fake.word())
-            article_list.append(article)
 
-        Article.objects.bulk_create(article_list, ignore_conflicts=True)
-
-    except Exception as e:
-        print(e)
+        except Exception as e:
+            print(e)
 
 
 def fake_comment(count=500):
