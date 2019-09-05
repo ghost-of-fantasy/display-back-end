d811766b1aa68d9a6e033e43b49197063bfd35ab
Niracler <1026037967@qq.com>
2019-09-05 11:20:19 +0800

    Use Retrieve in Article

--- apps/news/views.py
+++ apps/news/views.py
@@ -2,7 +2,7 @@ from django.db.models import Count
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, filters, status
 from rest_framework.pagination import PageNumberPagination
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, DestroyModelMixin, UpdateModelMixin
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, DestroyModelMixin, UpdateModelMixin
 from apps.news.filiters import ArticleFiliter
 from apps.news.models import Article
 from apps.news.serializers import ArticleSerializer,  TagSerializer, \
@@ -29,7 +29,7 @@ class TagsPagination(PageNumberPagination):
     max_page_size = 100
 
 
-class ArticleViewSet(ListModelMixin, viewsets.GenericViewSet, CreateModelMixin):
+class ArticleViewSet(ListModelMixin, viewsets.GenericViewSet, CreateModelMixin, RetrieveModelMixin):
     """文章管理API的视图"""
     queryset = Article.objects.all()
     pagination_class = ArticlePagination  # 分页函数
@@ -39,7 +39,7 @@ class ArticleViewSet(ListModelMixin, viewsets.GenericViewSet, CreateModelMixin):
     ordering_fields = ('id', 'publish_time', 'created')  # 排序
 
     def get_serializer_class(self):
-        if self.action == 'list':
+        if self.action == 'list' or self.action == 'retrieve':
             return ArticleSerializer
         elif self.action == 'create':
             return ArticleCreateSerializer
