7619677bb84e74ad119a93d9be3fb49bdb7a0ba2
niracler <1026037967@qq.com>
2019-10-09 21:19:38 +0800

    fix makemigrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,6 +13,7 @@ fi
 
 ## 这该死的删库命令
 #python manage.py flush --no-input
+python3 manage.py makemigrations
 python manage.py migrate
 python manage.py initadmin
 python manage.py collectstatic --no-input --clear
